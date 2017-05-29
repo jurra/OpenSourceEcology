2cf602378ef14c848bd9d02a11a4aa6850773780
jurra <jose.urra86@gmail.com>
2017-05-29 08:27:32 +0200

    Wordpress Theme copy

---
+++ wp-content/themes/twentyseventeen-child/template-parts/page/JoseTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Displays header media
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+<div class="custom-header">
+
+	<div class="custom-header-media has-header-video iframe">
+		 <iframe src="https://www.w3schools.com"></iframe>
+
+	</div>
+
+
+
+
+</div><!-- .custom-header -->
