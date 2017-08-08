97abde7d46e62f7fed77e2f76dc4a05be6a25cd4
Jose Carlos Urra Llanusa <jose.urra86@gmail.com>
2017-08-08 09:39:12 +0200

    Embedding 3D Model at the header of the page
    The idea is to show an interactive model of the 3D printer from the start and allow the user to interact with the model, and also engage with the workshops.

--- wp-content/themes/twentyseventeen/template-parts/header/header-image.php
+++ wp-content/themes/twentyseventeen/template-parts/header/header-image.php
@@ -10,10 +10,12 @@
 
 ?>
 <div class="custom-header">
+	<div class="sketchfab-embed-wrapper, custom-header-media"  style=""><iframe width="100%" height="100%" src="https://sketchfab.com/models/0902cdcf125747dcb948e36cf56de802/embed" frameborder="0"  allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
+</div>
 
-	<div class="custom-header-media">
-		<?php the_custom_header_markup(); ?>
-	</div>
+		<!-- <div class="custom-header-media">
+			<?php the_custom_header_markup(); ?>
+		</div> -->
 
 	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
 
